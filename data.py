302643fd05b74e25caf3ed58601fd4ec6a6e5889
Aron <aronpjansen@gmail.com>
2023-12-11 14:53:07 +0100

    Add dataloader generation

--- data.py
+++ data.py
@@ -7,7 +7,7 @@ from typing import Dict, List, Tuple
 import h5py
 import numpy as np
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import ConcatDataset, Dataset, random_split
 
 
 @dataclass
@@ -82,3 +82,117 @@ class XRayDataset(Dataset):
             labels = torch.from_numpy(labels)
 
             return data, labels
+
+
+def create_dataloaders(
+    hdf5_path: str,
+    train_samples: List[str],
+    height_range: Tuple[int, int],
+    train_day_range: Tuple[int, int],
+    validation_split: float,
+    seed: int = 42,
+    batch_size: int = 1,
+) -> Dict[str, DataSelection]:
+    """
+    Create train/validation/test split of data.
+
+    Selections of samples and days are made for the training/validation sets, the remaining are
+    used for the test set.
+    A selection of heights is made across all datasets.
+
+    The test set is split into two parts:
+    - test_strict: no overlap in samples or days with training/validation sets
+    - test_overlap: overlap in either samples or days with training/validation sets
+
+    Args:
+        hdf5_path: path to hdf5 file containing all data
+        train_samples: list of sample names to use for training
+        height_range: tuple of (min, max) height indices to use
+        train_day_range: tuple of (min, max) day indices to use for training
+        validation_split: fraction of training data to use for validation
+        seed: random seed for splitting data
+        batch_size: batch size for dataloaders
+
+    Returns:
+        dataloaders: dict of dataloaders
+            The keys are 'train', 'val', 'test_strict', 'test_overlap'
+    """
+    datasets = {}
+    train_val_selection = DataSelection(
+        sample_list=train_samples, height_range=height_range, day_range=train_day_range
+    )
+    train_val_dataset = XRayDataset(
+        hdf5_path=hdf5_path, data_selection=train_val_selection, name='train_val'
+    )
+
+    # split train/val randomly
+    num_val_samples = int(validation_split * len(train_val_dataset))
+    num_train_samples = len(train_val_dataset) - num_val_samples
+    datasets['train'], datasets['val'] = random_split(
+        train_val_dataset, [num_train_samples, num_val_samples]
+    )
+    datasets['train'].name = 'train'
+    datasets['val'].name = 'val'
+
+    # find test set by removing train/val samples
+    with h5py.File(hdf5_path, 'r') as hdf5_file:
+        all_samples = get_all_group_paths(hdf5_file)
+        test_samples = list(set(all_samples) - set(train_samples))
+
+        total_days = hdf5_file[all_samples[0]]['data'].shape[0]
+        # Note: assumes training days start from 0
+        test_day_range = (train_day_range[1], total_days)
+
+    # The test set that has no overlaps in either samples or days
+    strict_test_selection = DataSelection(
+        sample_list=test_samples, height_range=height_range, day_range=test_day_range
+    )
+    datasets['test_strict'] = XRayDataset(
+        hdf5_path=hdf5_path, data_selection=strict_test_selection, name='test_strict'
+    )
+
+    # The test set that has overlaps in either samples or days
+    overlap_test_selection_same_days = DataSelection(
+        sample_list=test_samples, height_range=height_range, day_range=train_day_range
+    )
+    overlap_test_dataset_same_days = XRayDataset(
+        hdf5_path=hdf5_path,
+        data_selection=overlap_test_selection_same_days,
+        name='test_overlap_same_days',
+    )
+    overlap_test_selection_same_samples = DataSelection(
+        sample_list=train_samples, height_range=height_range, day_range=test_day_range
+    )
+    overlap_test_dataset_same_samples = XRayDataset(
+        hdf5_path=hdf5_path,
+        data_selection=overlap_test_selection_same_samples,
+        name='test_overlap_same_samples',
+    )
+
+    datasets['test_overlap'] = ConcatDataset(
+        [overlap_test_dataset_same_days, overlap_test_dataset_same_samples]
+    )
+    datasets['test_overlap'].name = 'test_overlap'
+
+    # turn into dataloaders
+    dataloaders = {}
+    for name, dataset in datasets.items():
+        dataloaders[name] = torch.utils.data.DataLoader(
+            dataset, batch_size=batch_size, shuffle=(name == 'train'), num_workers=4
+        )
+
+    return dataloaders
+
+
+def get_all_group_paths(hdf5_file):
+    leaf_group_paths = []
+
+    def check_leaf_group(name):
+        item = hdf5_file[name]
+        if isinstance(item, h5py.Group):
+            # Check if it has datasets as children
+            if any(isinstance(item[obj_name], h5py.Dataset) for obj_name in item):
+                leaf_group_paths.append(name)
+
+    hdf5_file.visit(check_leaf_group)
+    return leaf_group_paths
