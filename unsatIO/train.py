92b61284abdc9ef8d229c49275a9562598e6d815
Aron <aronpjansen@gmail.com>
2023-12-12 14:53:04 +0100

    Fixes to training, running now

--- unsatIO/train.py
+++ unsatIO/train.py
@@ -1,17 +1,17 @@
+from data import create_dataloaders
 import lightning as L
 from lightning.pytorch.loggers import WandbLogger
 from models import UltraLocalModel
 import torch
 import torch.nn.functional as F
 
-from data import create_dataloaders
-
 dataloaders = create_dataloaders(
-    hdf5_path='../data/data.h5',
+    hdf5_path='../../data/data.h5',
     train_samples=['maize/coarse/loose', 'maize/fine/dense'],
     height_range=(400, 1_200),
     train_day_range=(0, 7),
     validation_split=0.1,
+    batch_size=8,
 )
 
 model = UltraLocalModel(input_size=1, hidden_sizes=[16, 32, 32], output_size=5)
@@ -21,26 +21,25 @@ class LTest(L.LightningModule):
     def __init__(self, model):
         super().__init__()
         self.model = model
-        self.loss = torch.nn.CrossEntropyLoss(reduction="mean")
 
     def training_step(self, batch, batch_idx):
         x, y = batch
-        # convert y to one-hot
-        y = torch.nn.functional.one_hot(y, num_classes=5).type(torch.float32)
+        # labels shouldn't have the channels dimension
+        y = torch.reshape(y, y.shape[:-1])
         y_hat = self.model(x)
-        y = torch.reshape(y, (-1, 5))
         y_hat = torch.reshape(y_hat, (-1, 5))
-        loss = self.loss(y, y_hat)
+        y = torch.reshape(y, (-1,))
+        loss = F.cross_entropy(y_hat, y)
         self.log("train/loss", loss)
         print(f"loss: {loss}")
         return loss
 
     def configure_optimizers(self):
-        return torch.optim.Adam(self.model.parameters(), lr=1e-3)
+        return torch.optim.Adam(self.model.parameters(), lr=1e-4)
 
 
 model_L = LTest(model)
-logger = WandbLogger(project="UNSAT", name="myrun")
+logger = WandbLogger(project="local-model")
 
-trainer = L.Trainer(log_every_n_steps=10, accelerator='cpu', logger=logger)
-trainer.fit(model_L, dataloaders['train'])
+trainer = L.Trainer(log_every_n_steps=1, accelerator='cpu', logger=logger)
+trainer.fit(model_L, dataloaders['train'], dataloaders['val'])
