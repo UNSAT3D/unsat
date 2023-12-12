58fa45995446bb909099fc60862f58d022220a81
Aron <aronpjansen@gmail.com>
2023-12-12 13:20:03 +0100

    assert_allclose

--- tests/test_data.py
+++ tests/test_data.py
@@ -48,15 +48,15 @@ def test_dataset(create_test_h5, remove_test_h5):
 
     # test some values
     with h5py.File(create_test_h5, 'r') as hdf5_file:
-        np.testing.assert_equal(
+        np.testing.assert_allclose(
             dataset[0][0].numpy(), hdf5_file['coarse/loose/04']['data'][0, 5], atol=1e-6
         )
         # second day
-        np.testing.assert_equal(
+        np.testing.assert_allclose(
             dataset[5][0].numpy(), hdf5_file['coarse/loose/04']['data'][1, 5], atol=1e-6
         )
         # second sample
-        np.testing.assert_equal(
+        np.testing.assert_allclose(
             dataset[10][0].numpy(), hdf5_file['fine/dense/07']['data'][0, 5], atol=1e-6
         )
 
